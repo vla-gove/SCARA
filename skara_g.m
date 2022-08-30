65ac1bc412344c2fe0118347fae4351f1664d17b
vla-gove <107414426+vla-gove@users.noreply.github.com>
2022-08-30 16:57:19 +0200

    Update skara_g.m

--- skara_g.m
+++ skara_g.m
@@ -1,7 +1,11 @@
+%defining individual links
 L(1) = Link('d', 1, 'a', 1, 'alpha', 0);
 L(2) = Link('d', 1, 'a', 1, 'alpha', 0);
 L(3) = Link('theta', 0, 'a', 0, 'alpha', 0);
-skara=SerialLink(L, 'name', 'skara_g');
-skara.fkine([0 0 0])
-skara.plot([0 0 0],'workspace',[-5 5 -5 5 -5 5]);
+%defining the robot
+scara=SerialLink(L, 'name', 'skara_g');
+%forward kinematics for joint parameters [0 0 0]
+scara.fkine([0 0 0])
+%plotting the robot position for joint parameters of [0 0 0] and defining the workspace
+scara.plot([0 0 0],'workspace',[-5 5 -5 5 -5 5]);
 
