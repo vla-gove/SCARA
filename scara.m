5ef9635e679f89eeaf15de5f835248eb26028ea9
vla-gove <107414426+vla-gove@users.noreply.github.com>
2022-08-30 16:57:40 +0200

    Update and rename skara_g.m to scara.m

--- scara.m
+++ scara.m
@@ -3,7 +3,7 @@ L(1) = Link('d', 1, 'a', 1, 'alpha', 0);
 L(2) = Link('d', 1, 'a', 1, 'alpha', 0);
 L(3) = Link('theta', 0, 'a', 0, 'alpha', 0);
 %defining the robot
-scara=SerialLink(L, 'name', 'skara_g');
+scara=SerialLink(L, 'name', 'scara');
 %forward kinematics for joint parameters [0 0 0]
 scara.fkine([0 0 0])
 %plotting the robot position for joint parameters of [0 0 0] and defining the workspace
