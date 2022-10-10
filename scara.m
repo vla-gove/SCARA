502ba7a6d31d3de3f637e9f975e11dad050fa8b3
vla-gove <107414426+vla-gove@users.noreply.github.com>
2022-10-10 13:41:50 +0200

    Update scara.m

--- scara.m
+++ scara.m
@@ -1,11 +1,12 @@
+startup_rvc;
 %defining individual links
 L(1) = Link('d', 1, 'a', 1, 'alpha', 0);
 L(2) = Link('d', 1, 'a', 1, 'alpha', 0);
 L(3) = Link('theta', 0, 'a', 0, 'alpha', 0);
 %defining the robot
-scara=SerialLink(L, 'name', 'scara');
+scararob=SerialLink(L, 'name', 'scara');
 %forward kinematics for joint parameters [0 0 0]
-scara.fkine([0 0 0])
+scararob.fkine([0 0 0])
 %plotting the robot position for joint parameters of [0 0 0] and defining the workspace
-scara.plot([0 0 0],'workspace',[-5 5 -5 5 -5 5]);
+scararob.plot([0 0 0],'workspace',[-5 5 -5 5 -5 5]);
 
